ad31003a96911599d8eff3c53bdd6587abab7819
Thomas Wiltshire <thomaswiltshire@Kochanski.local>
2017-03-01 15:35:59 +1100

    First Commit.

---
+++ DebugExercise/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char char_one = 'a';
+	char char_two = 'b';
+	char char_three = 'c';
+
+	int number_one = 0;
+	int number_two = 10;
+	int number_three = number_one + number_two;
+	int number_four = (((char_one + number_one) * number_three) * 6 - char_two);
+
+	float float_one = number_two + 16;
+
+	char_two--;
+	cin >> char_three;	
+	
+	return 0;
+}
